b7084e4fd9deb01f73d024d2bc0d008576f9903a
Leonardo <>
2020-12-01 12:13:28 -0300

    create personagem model

---
+++ src/Base/Model/Personagem.java
@@ -0,0 +1,15 @@
+package Base.Model;
+
+public class Personagem {
+    private String nome;
+    private String raca;
+    private String profissao;
+    private Integer mana;
+    private Integer ataque;
+    private Integer defesa;
+    private Integer defesa_magica;
+    private Integer velocidade;
+    private Integer destreza;
+    private Integer experiencia;
+    private Integer nivel_atual;
+}
