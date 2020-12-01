1b16576d2341b7dee05bddcaa8f373d6a86ac042
Leonardo <>
2020-12-01 12:34:35 -0300

    add toString and constructor to model

--- src/Base/Model/Personagem.java
+++ src/Base/Model/Personagem.java
@@ -12,4 +12,35 @@ public class Personagem {
     private Integer destreza;
     private Integer experiencia;
     private Integer nivel_atual;
+
+    public Personagem(String nome, String raca, String profissao, Integer mana, Integer ataque, Integer defesa, Integer defesa_magica, Integer velocidade, Integer destreza, Integer experiencia, Integer nivel_atual) {
+        this.nome = nome;
+        this.raca = raca;
+        this.profissao = profissao;
+        this.mana = mana;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.defesa_magica = defesa_magica;
+        this.velocidade = velocidade;
+        this.destreza = destreza;
+        this.experiencia = experiencia;
+        this.nivel_atual = nivel_atual;
+    }
+
+    @Override
+    public String toString() {
+        return "-----------Personagem----------" +'\n' +
+                "Nome: " + nome + '\n' +
+                "Raça: " + raca + '\n' +
+                "Profissão: " + profissao + '\n' +
+                "Mana: " + mana + '\n' +
+                "Ataque: " + ataque +'\n' +
+                "Defesa: " + defesa +'\n' +
+                "Defesa Mágica: " + defesa_magica +'\n' +
+                "Velocidade: " + velocidade +'\n' +
+                "Destreza: " + destreza +'\n' +
+                "Experiência: " + experiencia +'\n' +
+                "Nível Atual: " + nivel_atual +'\n' +
+                "-------------------------------";
+    }
 }
